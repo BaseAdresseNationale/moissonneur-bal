8e82d8919d55328992e1cf8b15d6e05f9e8a7b27
Jérôme Desboeufs <jerome.desboeufs@gmail.com>
2019-04-15 18:01:00 +0200

    Fix GPSO importer

--- importers/grand-paris-seine-ouest/index.js
+++ importers/grand-paris-seine-ouest/index.js
@@ -1,7 +1,8 @@
 function nomVoie(data) {
-  const prefixe = `${data.num || ''} ${data.rep || ''}`.trim()
-  const suffixe = `${data.cp} ${data.commune}`
-  return data.ident.substring(prefixe.length + 1, data.ident.length - suffixe.length - 1)
+  const {num, rep, cp, commune, ident} = data._default
+  const prefixe = `${num || ''} ${rep || ''}`.trim()
+  const suffixe = `${cp} ${commune}`
+  return ident.substring(prefixe.length + 1, ident.length - suffixe.length - 1)
 }
 
 module.exports = {nomVoie}
