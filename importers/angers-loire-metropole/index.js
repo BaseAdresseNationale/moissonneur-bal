082284799790f5d3675c6a46869729b304629bf5
Jérôme Desboeufs <jerome.desboeufs@gmail.com>
2019-07-10 15:04:58 +0200

    Base Adresse Locale d'Angers

--- importers/angers-loire-metropole/index.js
+++
@@ -1,8 +0,0 @@
-function dateMAJ(data) {
-  const {date_maj} = data._default
-  if (date_maj) {
-    return `${date_maj.substr(6, 4)}-${date_maj.substr(3, 2)}-${date_maj.substr(0, 2)}`
-  }
-}
-
-module.exports = {dateMAJ}
