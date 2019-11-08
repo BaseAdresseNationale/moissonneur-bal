57d7786f10d87281944f812b4bf8a413038c80ed
Jérôme Desboeufs <jerome.desboeufs@gmail.com>
2019-11-09 00:44:37 +0100

    Improve Paris importer

--- importers/paris/index.js
+++ importers/paris/index.js
@@ -1,13 +1,7 @@
-function nomVoie(data) {
-  const {l_nvoie, l_adr} = data._default
-  const nomVoie = l_adr.substr(l_nvoie.length).trim()
-  return nomVoie
-}
-
 function codeCommune(data) {
   if (data._default.c_ar) {
     return String(75100 + data._default.c_ar)
   }
 }
 
-module.exports = {nomVoie, codeCommune}
+module.exports = {codeCommune}
