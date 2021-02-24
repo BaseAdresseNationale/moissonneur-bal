bfbbac3f5a04bbca7b0b22b0be195eaa4ed6309b
Jérôme Desboeufs <jerome.desboeufs@gmail.com>
2021-02-25 00:20:15 +0100

    Remove dist output

--- build.js
+++ build.js
@@ -1,14 +1,11 @@
 #!/usr/bin/env node --max_old_space_size=8192
 require('dotenv').config()
-const {join} = require('path')
-const {emptyDir} = require('fs-extra')
 const Keyv = require('keyv')
 const bluebird = require('bluebird')
 const {uniq, compact} = require('lodash')
 const chalk = require('chalk')
 const {expandMetaWithResults} = require('./lib/meta')
 const {getCommune} = require('./lib/cog')
-const {createCsvFilesWriter} = require('./lib/exports/csv')
 const {computeList} = require('./lib/sources')
 const {processSource} = require('./lib/processing')
 const mongo = require('./lib/util/mongo')
@@ -16,7 +13,6 @@ const {endFarms} = require('./lib/util/farms')
 const {notify} = require('./lib/util/slack')
 
 const db = new Keyv('sqlite://bal.sqlite')
-const distPath = join(__dirname, 'dist')
 
 async function main() {
   await mongo.connect()
@@ -25,9 +21,6 @@ async function main() {
   const globalCommunes = new Set()
   let adressesCount = 0
   let erroredAdressesCount = 0
-
-  await emptyDir(distPath)
-  const csvFiles = createCsvFilesWriter(distPath)
 
   await db.clear()
 
@@ -56,7 +49,6 @@ async function main() {
       await db.set(`${source.meta.id}-report`, report)
     }
 
-    data.forEach(r => csvFiles.writeRow(r))
     adressesCount += data.length
     codesCommunes.forEach(c => globalCommunes.add(c))
     clearInterval(interval)
@@ -64,8 +56,6 @@ async function main() {
   }, {concurrency: 8})
 
   await db.set('datasets', compact(datasets))
-
-  await csvFiles.finish()
 
   /* Compute and display metrics */
 
