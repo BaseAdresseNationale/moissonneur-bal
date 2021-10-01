e796b36493a7662273352d730d07785bfb747677
Jérôme Desboeufs <jerome.desboeufs@gmail.com>
2021-10-01 22:22:31 +0200

    Remove bal.sqlite output

--- build.js
+++ build.js
@@ -1,9 +1,8 @@
 #!/usr/bin/env node --max_old_space_size=8192
 require('dotenv').config()
 const {join} = require('path')
-const Keyv = require('@livingdata/keyv')
 const bluebird = require('bluebird')
-const {uniq, compact} = require('lodash')
+const {uniq} = require('lodash')
 const chalk = require('chalk')
 const {expandMetaWithResults} = require('./lib/meta')
 const {computeList} = require('./lib/sources')
@@ -12,16 +11,12 @@ const mongo = require('./lib/util/mongo')
 const {endFarms} = require('./lib/util/farms')
 const {outputFile} = require('fs-extra')
 
-const db = new Keyv('sqlite://bal.sqlite')
-
 async function main() {
   await mongo.connect()
 
   const sources = await computeList()
 
-  await db.clear()
-
-  const datasets = await bluebird.map(sources, async source => {
+  await bluebird.map(sources, async source => {
     const interval = setInterval(() => console.log(`processing ${source.meta.title}`), 60000)
     const {originalFile, rows, errored, report} = await processSource(source)
 
@@ -47,15 +42,9 @@ async function main() {
 
     expandMetaWithResults(source.meta, {rows, report, errored})
 
-    if (report) {
-      await db.set(`${source.meta.id}-report`, report)
-    }
-
     clearInterval(interval)
     return source.meta
   }, {concurrency: 8})
-
-  await db.set('datasets', compact(datasets))
 
   endFarms()
   await mongo.disconnect()
