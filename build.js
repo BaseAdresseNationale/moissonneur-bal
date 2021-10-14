bdd1aef5daba1e65848e07e69b77e9dbf799c13f
Jérôme Desboeufs <jerome.desboeufs@gmail.com>
2021-10-15 00:15:15 +0200

    Simplify processSource

--- build.js
+++ build.js
@@ -13,26 +13,20 @@ const {fetchAllIfUpdated} = require('./lib/resources')
 const extractAdressesCustom = require('./lib/converters/custom').importData
 const extractAdressesBAL = require('./lib/converters/bal').importData
 
-function extractAdresses(source) {
-  if (source.converter) {
-    return extractAdressesCustom(source)
-  }
-
-  if (source.url) {
-    return extractAdressesBAL(source)
-  }
-
-  throw new Error('Configuration incomplète')
-}
-
 async function processSource(source) {
   try {
+    // Téléchargement des ressources
     const flattenedResources = Object.keys(source.resources)
       .map(resourceName => ({name: resourceName, ...source.resources[resourceName]}))
-
     const fetchedResources = await fetchAllIfUpdated(flattenedResources)
     source.resources = keyBy(fetchedResources, 'name')
-    return await extractAdresses(source)
+
+    // Conversion éventuelle des adresses au format BAL
+    if (source.converter) {
+      return await extractAdressesCustom(source)
+    }
+
+    return await extractAdressesBAL(source)
   } catch (error) {
     console.log(chalk.red(`${source.meta.title} - Impossible d’accéder aux adresses`))
     console.log(error)
