e3dba451058471080b4301cce62eb2e5c118304a
Jérôme Desboeufs <jerome.desboeufs@gmail.com>
2021-10-15 00:59:49 +0200

    Move resources definition to build step

--- build.js
+++ build.js
@@ -9,23 +9,27 @@ const {computeList} = require('./lib/sources')
 const mongo = require('./lib/util/mongo')
 const {endFarms} = require('./lib/util/farms')
 const {gzip} = require('./lib/util/gzip')
-const {fetchAllIfUpdated} = require('./lib/resources')
-const convert = require('./lib/convert')
+const {fetchAllIfUpdated, fetchIfUpdated} = require('./lib/resources')
+const {convert, getResourcesDefinitions} = require('./lib/convert')
+
+async function fetchAndConvert(source) {
+  const {converter} = source
+  // Téléchargement des ressources
+  const resourcesDefinitions = getResourcesDefinitions(converter)
+  const flattenedResources = Object.keys(resourcesDefinitions)
+    .map(resourceName => ({name: resourceName, ...resourcesDefinitions[resourceName]}))
+  const fetchedResources = await fetchAllIfUpdated(flattenedResources)
+  return convert(converter, keyBy(fetchedResources, 'name'))
+}
 
 async function processSource(source) {
   try {
-    // Téléchargement des ressources
-    const flattenedResources = Object.keys(source.resources)
-      .map(resourceName => ({name: resourceName, ...source.resources[resourceName]}))
-    const fetchedResources = await fetchAllIfUpdated(flattenedResources)
-    source.resources = keyBy(fetchedResources, 'name')
-
-    // Conversion éventuelle des adresses au format BAL
     if (source.converter) {
-      return await convert(source)
+      return await fetchAndConvert(source)
     }
 
-    return {originalFile: source.resources.default.data}
+    const resource = await fetchIfUpdated({url: source.url})
+    return {originalFile: resource.data}
   } catch (error) {
     console.log(chalk.red(`${source.meta.title} - Impossible d’accéder aux adresses`))
     console.log(error)
