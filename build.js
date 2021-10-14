8c2abcf2ce958a6aed204386191b5acd4171bbbf
Jérôme Desboeufs <jerome.desboeufs@gmail.com>
2021-10-15 00:12:38 +0200

    Move processSource to build file

--- build.js
+++ build.js
@@ -1,13 +1,43 @@
 #!/usr/bin/env node --max_old_space_size=8192
 require('dotenv').config()
 const {join} = require('path')
+const {keyBy} = require('lodash')
+const chalk = require('chalk')
 const {outputFile} = require('fs-extra')
 const bluebird = require('bluebird')
 const {computeList} = require('./lib/sources')
-const {processSource} = require('./lib/processing')
 const mongo = require('./lib/util/mongo')
 const {endFarms} = require('./lib/util/farms')
 const {gzip} = require('./lib/util/gzip')
+const {fetchAllIfUpdated} = require('./lib/resources')
+const extractAdressesCustom = require('./lib/converters/custom').importData
+const extractAdressesBAL = require('./lib/converters/bal').importData
+
+function extractAdresses(source) {
+  if (source.converter) {
+    return extractAdressesCustom(source)
+  }
+
+  if (source.url) {
+    return extractAdressesBAL(source)
+  }
+
+  throw new Error('Configuration incomplète')
+}
+
+async function processSource(source) {
+  try {
+    const flattenedResources = Object.keys(source.resources)
+      .map(resourceName => ({name: resourceName, ...source.resources[resourceName]}))
+
+    const fetchedResources = await fetchAllIfUpdated(flattenedResources)
+    source.resources = keyBy(fetchedResources, 'name')
+    return await extractAdresses(source)
+  } catch (error) {
+    console.log(chalk.red(`${source.meta.title} - Impossible d’accéder aux adresses`))
+    console.log(error)
+  }
+}
 
 async function main() {
   await mongo.connect()
