8ede28297b21e4af8cb3323c413a0f7dc0367087
Jérôme Desboeufs <jerome.desboeufs@gmail.com>
2021-10-15 22:14:44 +0200

    Handle fetchArtifacts

--- build.js
+++ build.js
@@ -1,7 +1,7 @@
 #!/usr/bin/env node --max_old_space_size=8192
 require('dotenv').config()
 const {join} = require('path')
-const {keyBy} = require('lodash')
+const {keyBy, omit} = require('lodash')
 const chalk = require('chalk')
 const {outputFile} = require('fs-extra')
 const bluebird = require('bluebird')
@@ -15,29 +15,63 @@ const {fetchAllIfUpdated, fetchIfUpdated} = require('./lib/resources')
 const {convert, getResourcesDefinitions} = require('./lib/convert')
 
 const Source = require('./lib/models/source')
+const Harvest = require('./lib/models/harvest')
 
-async function fetchIfUpdatedAndConvert(source) {
+async function fetchIfUpdatedAndConvert(source, indexedFetchArtefacts) {
   const {converter} = source
-  // Téléchargement des ressources
+
   const resourcesDefinitions = getResourcesDefinitions(converter)
+
   const flattenedResources = Object.keys(resourcesDefinitions)
-    .map(resourceName => ({name: resourceName, ...resourcesDefinitions[resourceName]}))
+    .map(resourceName => {
+      const resourceDefinition = resourcesDefinitions[resourceName]
+      const relatedFetchArtefact = indexedFetchArtefacts[resourceDefinition.url] || {}
+
+      return {
+        name: resourceName,
+        ...relatedFetchArtefact,
+        ...resourceDefinition
+      }
+    })
   const fetchedResources = await fetchAllIfUpdated(flattenedResources)
-  return convert(converter, keyBy(fetchedResources, 'name'))
+
+  if (fetchedResources.some(r => r.data)) {
+    const convertedFile = await convert(converter, keyBy(fetchedResources, 'name'))
+    return {
+      fetchArtefacts: fetchedResources.map(r => omit(r, 'data')),
+      updateStatus: 'updated',
+      newFile: convertedFile
+    }
+  }
+
+  return {
+    fetchArtefacts: fetchedResources,
+    updateStatus: 'unchanged'
+  }
 }
 
 async function processSource(source) {
+  const lastCompletedHarvest = await Harvest.getLastCompletedHarvest(source._id)
+  const {fetchArtefacts: previousFetchArtefacts} = lastCompletedHarvest || {}
+  const indexedFetchArtefacts = keyBy(previousFetchArtefacts, 'url')
+
   try {
     if (source.converter) {
-      return await fetchIfUpdatedAndConvert(source)
+      return await fetchIfUpdatedAndConvert(source, indexedFetchArtefacts)
     }
 
-    const resource = await fetchIfUpdated({url: source.url})
-    return {originalFile: resource.data}
+    const relatedFetchArtefact = indexedFetchArtefacts[source.url] || {}
+    const resource = await fetchIfUpdated({...relatedFetchArtefact, url: source.url})
+
+    return {
+      fetchArtefacts: [omit(resource, 'data')],
+      updateStatus: resource.data ? 'updated' : 'unchanged',
+      newFile: resource.data || undefined
+    }
   } catch (error) {
     console.log(chalk.red(`${source.title} - Impossible d’accéder aux adresses`))
     console.log(error.message)
-    return {}
+    return {error: error.message}
   }
 }
 
@@ -58,19 +92,21 @@ async function main() {
   await bluebird.map(sourcesToHarvest, async source => {
     await Source.startHarvesting(source._id)
 
-    const {originalFile} = await processSource(source)
+    const {fetchArtefacts, updateStatus, newFile, error} = await processSource(source)
 
-    if (!originalFile) {
-      await Source.finishHarvesting(source._id, 'failed')
+    if (error) {
+      await Source.finishHarvesting(source._id, {status: 'failed', error})
       return
     }
 
-    await outputFile(
-      join(__dirname, 'dist', `${source._id}.csv.gz`),
-      await gzip(originalFile)
-    )
+    if (newFile) {
+      await outputFile(
+        join(__dirname, 'dist', `${source._id}.csv.gz`),
+        await gzip(newFile)
+      )
+    }
 
-    await Source.finishHarvesting(source._id, 'completed')
+    await Source.finishHarvesting(source._id, {status: 'completed', fetchArtefacts, updateStatus})
   }, {concurrency: 8})
 
   endFarms()
