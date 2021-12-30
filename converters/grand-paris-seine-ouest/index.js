7ca6256b0be9e9dff773fa396128dbbbaed85110
Jérôme Desboeufs <jerome.desboeufs@gmail.com>
2021-12-30 15:46:30 +0100

    Suppression source custom GPSO

--- converters/grand-paris-seine-ouest/index.js
+++
@@ -1,8 +0,0 @@
-function voie_nom(data) {
-  const {num, rep, cp, commune, ident} = data._default
-  const prefixe = `${num || ''} ${rep || ''}`.trim()
-  const suffixe = `${cp} ${commune}`
-  return ident.substring(prefixe.length + 1, ident.length - suffixe.length - 1)
-}
-
-module.exports = {voie_nom}
