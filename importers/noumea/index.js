d7b63b30e512f9b8c5ce45deef08b55eb503af6e
Jérôme Desboeufs <jerome.desboeufs@gmail.com>
2022-07-04 15:35:16 +0200

    Fix noumea importer

--- importers/noumea/index.js
+++ importers/noumea/index.js
@@ -1,13 +1,5 @@
 function codeCommune() {
-  return '38185'
+  return '98818'
 }
 
-function nomVoie(data) {
-  return [data._default.VOIE_TYPE, data._default.VOIE_ARTICLE, data._default.VOIE_LIBEL]
-    .filter(p => Boolean(p))
-    .join(' ')
-    .replace(/'\s/g, '\'')
-    .replace(/’\s/g, '’')
-}
-
-module.exports = {codeCommune, nomVoie}
+module.exports = {codeCommune}
