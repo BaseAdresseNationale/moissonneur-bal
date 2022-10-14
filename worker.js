9f6eeea7759288ad27cc19cb7bfde88fcfc76e4b
Jérôme Desboeufs <jerome.desboeufs@gmail.com>
2022-10-14 23:22:27 +0200

    Move stalled harvests cleanup in its own job

--- worker.js
+++ worker.js
@@ -7,12 +7,18 @@ const mongo = require('./lib/util/mongo')
 const {runWorkflow} = require('./lib/worker/run-workflow')
 const {harvestRequestedSources} = require('./lib/worker/start-harvest')
 const {updateSources} = require('./lib/worker/update-sources')
+const {cleanStalledHarvests} = require('./lib/worker/clean-stalled-harvests')
 
 const jobsList = [
   {
     name: 'mise à jour des sources de données',
     every: '5m',
     handler: updateSources
+  },
+  {
+    name: 'nettoyage des moissonnages bloqués',
+    every: '2m',
+    handler: cleanStalledHarvests
   },
   {
     name: 'lancement du workflow',
