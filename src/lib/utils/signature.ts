4dee75315fc515b4a4e76a0265e9bfdc75a5ae58
Fufeck <fabien.tafforeau@gmail.com>
2024-06-10 14:49:48 +0200

    fix Provide a compare function to avoid sorting elements alphabetically

--- src/lib/utils/signature.ts
+++ src/lib/utils/signature.ts
@@ -2,10 +2,12 @@ import stringify from 'fast-json-stable-stringify';
 import hasha from 'hasha';
 
 export function signData(data: any[]): string {
-  const signedRows = data.map((row) =>
+  const signedRows: string[] = data.map((row) =>
     hasha(stringify(row), { algorithm: 'md5' }),
   );
-  const sortedSignatures = signedRows.sort().join('|');
+  const sortedSignatures = signedRows
+    .sort((a: string, b: string) => a.localeCompare(b))
+    .join('|');
 
   return hasha(sortedSignatures, { algorithm: 'sha256' });
 }
