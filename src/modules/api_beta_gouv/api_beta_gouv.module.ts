d77409c686d96616fd067a5ecd83e54b4c64a9a0
Fufeck <fabien.tafforeau@gmail.com>
2024-10-14 17:58:47 +0200

    linter

--- src/modules/api_beta_gouv/api_beta_gouv.module.ts
+++ src/modules/api_beta_gouv/api_beta_gouv.module.ts
@@ -1,4 +1,4 @@
-import { Logger, Module } from '@nestjs/common';
+import { Module } from '@nestjs/common';
 
 import { ApiBetaGouvService } from './api_beta_gouv.service';
 import { ConfigModule, ConfigService } from '@nestjs/config';
